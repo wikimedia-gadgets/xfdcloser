8fbd40a907e91d918b05dbd3be3f13d1cc9f243e
Evad37 <evad37@gmail.com>
2020-01-30 22:02:07 +0800

    Add concat step to build, to apply comments to minified script

---
+++ core-comment-top.js
@@ -0,0 +1,20 @@
+/*  _______________________________________________________________________________
+ * |                                                                               |
+ * |                     === WARNING: GLOBAL GADGET FILE ===                       |
+ * |                   Changes to this page affect many users.                     |
+ * |  Please discuss changes on the talk page or on [[WT:Gadget]] before editing.  |
+ * |_______________________________________________________________________________|
+ * 
+ * Built from source code at GitHub repository [https://github.com/evad37/xfdcloser]
+ * All changes should be made in the repository, otherwise they will be lost.
+ * 
+ * To update this script from github, you must have a local repository set up. Then
+ * follow the instructions at [https://github.com/evad37/xfdcloser/blob/master/README.md]
+ * 
+ * XFDcloser: close deletion discussions at XfD venues, including actions to implement closes; see [[WP:XFDC]]
+ * Author: Evad37
+ * Licencing and attribution: [[WP:XFDC#Licencing and attribution]]
+ */
+/* jshint esversion: 5, laxbreak: true, undef: true, eqnull: true, maxerr: 3000 */
+/* globals console, document, window, $, mw, OO, extraJs, Morebits */
+/* <nowiki> */
