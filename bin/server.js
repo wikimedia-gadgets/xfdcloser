e356a9c27f1df90bd6986af364d37a3cb887ae7f
Evad37 <evad37@gmail.com>
2021-01-19 17:06:58 +0800

    Fix lint warnings in bin scripts

--- bin/server.js
+++ bin/server.js
@@ -1,23 +1,23 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 8125;
 
 const server = http.createServer((request, response) => {
-	const filePath = '.' + request.url
+	const filePath = "." + request.url;
 	const contentType = /\.css$/.test(filePath) ? "text/css" : "text/javascript";
 	fs.readFile(filePath, function(error, content) {
 		if (error) {
-			response.writeHead(error.code == 'ENOENT' ? 404 : 500);
-			response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+			response.writeHead(error.code == "ENOENT" ? 404 : 500);
+			response.end("Sorry, check with the site admin for error: "+error.code+" ..\n");
 		} else {
-			response.writeHead(200, { 'Content-Type': contentType });
-			response.end(content, 'utf-8');
+			response.writeHead(200, { "Content-Type": contentType });
+			response.end(content, "utf-8");
 		}
 	});
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
--- bin/testall.js
+++ bin/testall.js
@@ -1,11 +1,11 @@
 // This script replaces any left-over occurances .only within the test files. While .only is useful
 // when testing a specific file or function, when doing a build all tests should be run (except any
 // which are skipped).
-const replace = require('replace-in-file');
+const replace = require("replace-in-file");
 const options = {
-	files: 'test/*.js',
+	files: "test/*.js",
 	from: /(describe|it).only\(/g,
-	to: '$1(',
+	to: "$1(",
 	countMatches: true,
 };
 try {
@@ -14,5 +14,5 @@ try {
 		console.log(`Removed ${result.numReplacements} occcurance${result.numReplacements === 1 ? "" :"s"} of .only from ${result.file}`)
 	);
 } catch (error) {
-	console.error('Error occurred:', error);
+	console.error("Error occurred:", error);
 }
