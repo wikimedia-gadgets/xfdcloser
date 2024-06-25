3cbd29169e2bbc25500ff28624081ee34d1822d2
93Pd9s8Jt <92815501+93Pd9s8Jt@users.noreply.github.com>
2024-06-25 21:44:13 +0100

    Add chunking for API requests

--- xfdcloser-src/Controllers/DiscussionViewController.js
+++ xfdcloser-src/Controllers/DiscussionViewController.js
@@ -1,6 +1,6 @@
 import { $, mw, OO } from "../../globals";
 import API from "../api";
-import { dateFromSubpageName, windowOffsetTop } from "../util"; 
+import { dateFromSubpageName, windowOffsetTop } from "../util";
 import MainWindowModel from "../Models/MainWindowModel";
 import windowSetManager from "../windowSetManager";
 
@@ -16,34 +16,51 @@ class DiscussionViewController {
 		this.quickCloseButton = widget.quickCloseButtonMenu;
 		this.quickCloseMenu = widget.quickCloseButtonMenu.getMenu();
 
-		this.model.connect(this, {update: "updateFromModel"});
+		this.model.connect(this, { update: "updateFromModel" });
 
-		this.closeButton.connect(this, {click: ["onButtonClick", "close"]});
-		this.relistButton.connect(this, {click: ["onButtonClick", "relist"]});
-		this.quickCloseMenu.connect(this, {choose: "onQuickCloseChoose"});
+		this.closeButton.connect(this, { click: ["onButtonClick", "close"] });
+		this.relistButton.connect(this, { click: ["onButtonClick", "relist"] });
+		this.quickCloseMenu.connect(this, { choose: "onQuickCloseChoose" });
 
-		if ( this.model.pages.length ) {
+		if (this.model.pages.length) {
 			this.fetchInfoFromApi();
 		}
 	}
 	fetchInfoFromApi() {
-		const pagesExistencesPromise = API.get({
-			action: "query",
-			format: "json",
-			formatversion: 2,
-			titles: this.model.pagesNames,
-			prop: "info",
-			inprop: "talkid"
-		}).then(response => response.query.pages.forEach(page => {
-			const pageTitle = mw.Title.newFromText(page.title);
-			const talkpageTitle = pageTitle.getTalkPage();
-			mw.Title.exist.set(pageTitle.getPrefixedDb(), !page.missing);
-			if ( talkpageTitle ) {
-				mw.Title.exist.set(talkpageTitle.getPrefixedDb(), !!page.talkid);
-			}
-		}));
-		const nominationDatePromise = ( this.model.venue.type !== "afd" && this.model.venue.type !== "mfd" )
-			? $.Deferred().resolve( dateFromSubpageName(this.model.discussionSubpageName) )
+		const pageNames = this.model.pagesNames;
+		const chunkSize = pageNames.length <= 100 ? 50 : (await mw.user.getRights()).indexOf( 'apihighlimits' ) >= 0 ? 500 : 50;
+
+		const chunks = [];
+		for (let i = 0; i < pageNames.length; i += chunkSize) {
+			chunks.push(pageNames.slice(i, i + chunkSize));
+		}
+
+		const fetchChunk = (chunk) => {
+			return API.get({
+				action: "query",
+				format: "json",
+				formatversion: 2,
+				titles: chunk.join('|'),
+				prop: "info",
+				inprop: "talkid"
+			}).then(response => {
+				if (response.query && response.query.pages) {
+					response.query.pages.forEach(page => {
+						const pageTitle = mw.Title.newFromText(page.title);
+						const talkpageTitle = pageTitle.getTalkPage();
+						mw.Title.exist.set(pageTitle.getPrefixedDb(), !page.missing);
+						if (talkpageTitle) {
+							mw.Title.exist.set(talkpageTitle.getPrefixedDb(), !!page.talkid);
+						}
+					});
+				}
+			})
+		};
+
+		const fetchPromises = chunks.map(chunk => fetchChunk(chunk));
+		const pagesExistencesPromise = Promise.all(fetchPromises);
+		const nominationDatePromise = (this.model.venue.type !== "afd" && this.model.venue.type !== "mfd")
+			? $.Deferred().resolve(dateFromSubpageName(this.model.discussionSubpageName))
 			: API.get({
 				action: "query",
 				format: "json",
@@ -75,7 +92,7 @@ class DiscussionViewController {
 			$(`.${this.model.id}-discussion-node`).addClass("xfdc-actioned-discussion");
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param {String} type "close" or "relist" 
