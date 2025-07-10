031872b89f2342a0ec6e9b661f805de83633ceec
NovemLinguae <79697282+NovemLinguae@users.noreply.github.com>
2025-07-10 05:29:55 -0700

    deploy: add a `--quick` mode (#106)
    * deploy: add a `--quick` mode
    
    - deploys both beta and main
    - deploys with no prompting
    - converted promises to async, so that command line stuff runs sequentially instead of getting jumbled together
    - changed `npm run deploy` to use `--quick` mode
    
    * promises to async, fix beta:n bug, add npm run build
    
    * update readme, add example credentials file

--- bin/deploy.js
+++ bin/deploy.js
@@ -50,48 +50,92 @@ function logError(error) {
 	);
 }
 
-// Prompt user for info
-const wiki = prompt("> Wikipedia subdomain: ");
-const beta = prompt("> Beta deployment [Y/n]: ");
-const isBeta = beta.trim().toUpperCase() !== "N";
-console.log(`Targeting ${isBeta ? "BETA" : "MAIN"} version of script.`);
-const message = prompt("> Edit summary message (optional): ");
+// Check for --quick parameter. Quick mode deploys to both enwiki and enwiki-beta, without prompting for any info
+const quick = process.argv.includes("--quick");
+const config = [];
 
-// Extract info for edit summary.
-const version = require("../package.json").version;
-const sha = execSync("git rev-parse --short HEAD").toString("utf8").trim();
-const editSummary = `v${version} at ${sha}: ${message || "Updated from repository"}`;
-console.log(`Edit summary is: "${editSummary}"`);
+if (quick) {
+	config.push({
+		wiki: "en",
+		beta: "y",
+		userComment: "",
+		consoleMessage: "QUICK MODE: Using defaults (en, beta, blank edit summary, auto-continue)"
+	});
+	config.push({
+		wiki: "en",
+		beta: "n",
+		userComment: "",
+		consoleMessage: "QUICK MODE: Using defaults (en, main, blank edit summary, auto-continue)"
+	});
+} else {
+	// Prompt user for info
+	wiki = prompt("> Wikipedia subdomain: ");
+	beta = prompt("> Beta deployment [Y/n]: ");
+	userComment = prompt("> Edit summary message (optional): ");
+	config.push({
+		wiki: wiki,
+		beta: beta,
+		userComment: userComment,
+		consoleMessage: null
+	});
 
-const deployments = [
-	{file: "loader-gadget.js", target: "MediaWiki:Gadget-XFDcloser.js"},
-	{file: "core-gadget.js", target: `MediaWiki:Gadget-XFDcloser-core${isBeta ? "-beta" : ""}.js`},
-	{file: "styles-gadget.css", target: `MediaWiki:Gadget-XFDcloser-core${isBeta ? "-beta" : ""}.css`}
-];
+}
 
-const api = new mwn({
-	apiUrl: `https://${wiki}.wikipedia.org/w/api.php`,
-	username: username,
-	password: password
-});
+async function deploy(config) {
+	for (let i = 0; i < config.length; i++) {
+		const wiki = config[i].wiki;
+		const beta = config[i].beta;
+		const userComment = config[i].userComment;
+		const consoleMessage = config[i].consoleMessage;
 
-console.log(`... logging in as ${username}  ...`);
-api.loginGetToken().then(() => {
-	prompt("> Press [Enter] to start deploying or [ctrl + C] to cancel");
-	console.log("--- starting deployment ---");
-	const editPromises = deployments.map(deployment => {
-		let content = fs.readFileSync("./dist/"+deployment.file, "utf8").toString();
-		return api.save(deployment.target, content, editSummary).then((response) => {
-			const status = response && response.nochange
-				? "━ No change saving"
-				: "✔ Successfully saved";
-			console.log(`${status} ${deployment.file} to ${wiki}:${deployment.target}`);
-		}, (error) => {
-			console.log(`✘ Failed to save ${deployment.file} to ${wiki}:${deployment.target}`);
-			logError(error);
+		if (consoleMessage) {
+			console.log(consoleMessage);
+		}
+
+		// Extract info for edit summary.
+		const version = require("../package.json").version;
+		const sha = execSync("git rev-parse --short HEAD").toString("utf8").trim();
+		const editSummary = `v${version} at ${sha}: ${userComment || "Updated from repository"}`;
+		console.log(`Edit summary is: "${editSummary}"`);
+
+		const isBeta = beta.trim().toUpperCase() !== "N";
+		const deployments = [
+			{file: "loader-gadget.js", target: "MediaWiki:Gadget-XFDcloser.js"},
+			{file: "core-gadget.js", target: `MediaWiki:Gadget-XFDcloser-core${isBeta ? "-beta" : ""}.js`},
+			{file: "styles-gadget.css", target: `MediaWiki:Gadget-XFDcloser-core${isBeta ? "-beta" : ""}.css`}
+		];
+
+		const api = new mwn({
+			apiUrl: `https://${wiki}.wikipedia.org/w/api.php`,
+			username: username,
+			password: password
 		});
-	});
-	Promise.all(editPromises).then(() => {
-		console.log("--- end of deployment ---");
-	});
-}).catch(logError);
+
+		console.log(`... logging in as ${username}  ...`);
+		try {
+			await api.loginGetToken();
+			if (!quick) {
+				prompt("> Press [Enter] to start deploying or [ctrl + C] to cancel");
+			}
+			console.log("--- starting deployment ---");
+			for (const deployment of deployments) {
+				try {
+					let content = fs.readFileSync("./dist/" + deployment.file, "utf8").toString();
+					const response = await api.save(deployment.target, content, editSummary);
+					const status = response && response.nochange
+						? "━ No change saving"
+						: "✔ Successfully saved";
+					console.log(`${status} ${deployment.file} to ${wiki}:${deployment.target}`);
+				} catch (error) {
+					console.log(`✘ Failed to save ${deployment.file} to ${wiki}:${deployment.target}`);
+					logError(error);
+				}
+			}
+			console.log("--- end of deployment ---");
+		} catch (error) {
+			logError(error);
+		}
+	}
+}
+
+deploy(config);
