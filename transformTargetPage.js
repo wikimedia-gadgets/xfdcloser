eafb24c4120d412fc8e2af07188c56835ad31734
NovemLinguae <novemlinguae@gmail.com>
2026-07-23 05:58:01 -0700

    Cleanup merge templates in target article v3
    Related #135
    Related #151

---
+++ transformTargetPage.js
@@ -0,0 +1,39 @@
+// TODO: rename and move this file
+
+function execute() {
+	const listOfPagesToCheck = getListOfPagesToCheck(outcome, sourcePage, targetPage, afdPage, xfdType);
+	for ( const page of listOfPagesToCheck ) {
+		const oldWikicode = getOldWikicode(page);
+		const newWikicode = removeTagsFromTargetPage(oldWikicode, sourcePage, targetPage, nominationName, dateOfClosure);
+		if ( newWikicode !== oldWikicode ) {
+			saveNewWikicode(page, newWikicode);
+		}
+	}
+}
+
+function removeTagsFromTargetPage(oldWikicode, sourcePage, nominationName) {
+	let newWikicode = oldWikicode;
+
+	// {{Merge}}, {{merge from}}, {{Merge to}}, {{Article for deletion/dated}}, {{being merged to}}, {{being merged}}, {{being merged from}}, {{Merge portions from}}
+	const templates = [
+		"Merge", /* aliases: */ "Mergedisputed", "Mergewith", "MergeDisputed", "MergeVfD", "Merge-disputed", "Merge disputed", "Merge-multiple", "Mergesplit", "MergeSplit", "Mergemulti", "Mergetomultiple-with", "Multimerge", "Proposed merge", "Merge with",
+
+		"Merge from", /* aliases: */ "Merge-from", "Include", "Mergefrom-multiple", "Multiplemergefrom", "Mergefrommulti", "Mergefrommultiple", "Multimergefrom", "Mergefrom-category", "MergeFrom", "Mergefrom", "Merge from draft", "Merge from AfD",
+
+		"Merge to", /* aliases: */ "Merge-to", "Mergeinto", "MergePartial", "MergetoCat", "Mergelist", "Mergeto-disputed", "Mergeto-multiple", "Multiplemergeinto", "Multiplemergeto", "Multiple-merge-to", "Merge into", "MergeTo", "Mergeto", "Merge to article", "Merge2", "Merge-into",
+
+		"Article for deletion/dated", /* aliases: */ "AfDM", "Afd/dated", "AfD/dated", "Afdm",
+
+		"Being merged to", /* aliases: */ "Merging to", "Being Merge to", "Being merge to", "Merging into", "Merginginto", "Mergingto", "Merging-to",
+
+		"Being merged", /* aliases: */ "Merging", "Mergingsectionto",
+
+		"Being merged from", /* aliases: */ "Merging from", "Mergingfrom", "Being Merge from", "Being merge from", "Merging-from",
+
+		"Merge portions from", /* aliases: */ "Move section portions from", "Move portions from", "Merge section portions from",
+	];
+
+	// TODO: do things
+
+	return newWikicode;
+}
